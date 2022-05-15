e5676867ae5911873e3bb0c650a2346a904047ab
Laylson Sousa <laylsonss3@gmail.com>
2022-05-15 12:14:33 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,113 @@
+function Home() {
+    return(
+        <div>
+            <div className="body">
+                <div className="cabecalho">
+                    <h1>Design online</h1>
+                    
+                </div>
+                <div className="estilos">
+                    
+                    <div className="modelo-estilo">
+                    <img src="teste1.png" />
+
+
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste2.png" />
+
+                        
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste3.png" />
+                        
+                        <img className="premium" src="estrela.png" />
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste4.png" />
+
+                        
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste5.png" />
+
+                        
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste6.png" />
+
+                        
+                        <img className="premium" src="estrela.png" />
+
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste7.png" />
+
+                        
+                        <img className="premium" src="estrela.png" />
+
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste8.png" />
+
+                        
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste9.png" />
+
+                        
+                        <img className="premium" src="estrela.png" />
+
+                    </div>
+                    <div className="modelo-estilo">
+                    <img src="teste10.png" />
+
+                        
+                    </div>
+
+                </div>
+                <div className="main">
+                    <div className="principal">
+                        <div className="foto">
+                            <img src="foto-teste.png"></img>
+
+                            <div className="nome-candidato">
+                                Candidato Fulano
+                            </div>
+                            
+                            <div className="numero-candidato">
+                                12.345
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="menu">
+
+                        <input placeholder="Nome do candidato" type="text" />
+
+
+                        <input placeholder="Número" type="text" />
+                        
+                        <span>Configuração</span>
+
+                        <input type="range" />
+
+
+                        
+                        
+
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Home
