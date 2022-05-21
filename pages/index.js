6da4291f4a70e114ee6498b9298dd3ddee16ef70
Laylson Sousa <laylsonss3@gmail.com>
2022-05-21 10:07:51 -0300

    agora vai

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ function Home() {
 
                 </div>
 
-                <Atalhos />
+                {/* <Atalhos /> */}
 
                 <div className="main">
                     <div className="principal">
